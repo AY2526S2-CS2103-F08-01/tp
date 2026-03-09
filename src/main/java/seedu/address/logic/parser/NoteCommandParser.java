7b39ab6a3728efdc58999af0370a0ce07057ffa3
Ryan Goh <ryangoh206@gmail.com>
2026-03-10 01:16:48 +0800

    Add NoteCommand and NoteCommandParser

---
+++ src/main/java/seedu/address/logic/parser/NoteCommandParser.java
@@ -0,0 +1,42 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.NoteCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new NoteCommand object
+ */
+public class NoteCommandParser implements Parser<NoteCommand> {
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the NoteCommand and returns an
+     * NoteCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public NoteCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_NOTE);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE), pe);
+        }
+
+        if (!argMultimap.getValue(PREFIX_NOTE).isPresent()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, NoteCommand.MESSAGE_USAGE));
+        }
+
+        String noteContent = argMultimap.getValue(PREFIX_NOTE).get();
+
+        return new NoteCommand(index, noteContent);
+    }
+}
---
+++ src/test/java/seedu/address/logic/commands/NoteCommandTest.java
@@ -0,0 +1,57 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NOTE_AMY;
+import static seedu.address.logic.commands.CommandTestUtil.VALID_NOTE_BOB;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import static seedu.address.logic.commands.NoteCommand.MESSAGE_ARGUMENTS;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+
+public class NoteCommandTest {
+
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void execute() {
+        final String noteContent = "Some note.";
+
+        assertCommandFailure(new NoteCommand(INDEX_FIRST_PERSON, noteContent), model,
+                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), noteContent));
+    }
+
+    @Test
+    public void equals() {
+        final NoteCommand standardCommand = new NoteCommand(INDEX_FIRST_PERSON, VALID_NOTE_AMY);
+
+        // same values -> returns true
+        NoteCommand commandWithSameValues = new NoteCommand(INDEX_FIRST_PERSON, VALID_NOTE_AMY);
+        assertTrue(standardCommand.equals(commandWithSameValues));
+
+        // same object -> returns true
+        assertTrue(standardCommand.equals(standardCommand));
+
+        // null -> returns false
+        assertFalse(standardCommand.equals(null));
+
+        // different types -> returns false
+        assertFalse(standardCommand.equals(new ClearCommand()));
+
+        // different index -> returns false
+        assertFalse(standardCommand.equals(new NoteCommand(INDEX_SECOND_PERSON, VALID_NOTE_AMY)));
+
+        // different note -> returns false
+        assertFalse(standardCommand.equals(new NoteCommand(INDEX_FIRST_PERSON, VALID_NOTE_BOB)));
+    }
+
+
+
+}
